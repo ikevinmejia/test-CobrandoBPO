b17326b5dd5e4e367417d6ffe1960ce0fef5c304
Kevin Mejía <kevinarley6958@gmail.com>
2022-09-27 22:40:37 -0500

    deploy

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_APIKEY,
-    authDomain: process.env.REACT_APP_AUTHDOMAIN,
-    projectId: process.env.REACT_APP_PROJECTID,
-    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-    appId: process.env.REACT_APP_APPID,
-};
+    apiKey: "AIzaSyAfBzAy_rCpV7f22Gq9txUnt1DXMQXSN4E",
+    authDomain: "pruebacobrando.firebaseapp.com",
+    projectId: "pruebacobrando",
+    storageBucket: "pruebacobrando.appspot.com",
+    messagingSenderId: "448381129624",
+    appId: "1:448381129624:web:1176056ad95aaef5b091ed"
+  };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
